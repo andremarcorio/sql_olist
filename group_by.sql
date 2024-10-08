e9655f82a9b13f5fffffbe2caab8b7a885e3a109
Wilcer Andre Marcorio <andremarcorio@gmail.com>
2024-10-08 19:52:23 -0300

    Join

--- group_by.sql
+++ group_by.sql
@@ -17,7 +17,7 @@ order by max(product_description_lenght) desc
 -- Faça uma query que apresente o tamanho médio, máximo e mínimo do nome do objeto por categoria.
 
 select product_category_name as categoria,
-       avg(product_name_lenght) as media_nome,
+       round(avg(product_name_lenght),2) as media_nome,
        max(product_name_lenght) as maximo_nome,
        min(product_name_lenght) as minimo_nome
 
@@ -29,7 +29,7 @@ group by product_category_name
 -- Considere apenas os objetos que tenham a descrição maior que 100.
 
 select product_category_name as categoria,
-       avg(product_name_lenght) as media_nome,
+       round(avg(product_name_lenght),2) as media_nome,
        max(product_name_lenght) as maximo_nome,
        min(product_name_lenght) as minimo_nome
 
@@ -44,7 +44,7 @@ group by product_category_name
 -- Exiba apenas as categorias com tamanho médio de descrição do objeto maior que 500 caracteres.
 
 select product_category_name as categoria,
-       avg(product_name_lenght) as media_nome,
+       round(avg(product_name_lenght),2) as media_nome,
        max(product_name_lenght) as maximo_nome,
        min(product_name_lenght) as minimo_nome
 
